30e12c11642e5bfe27154dda5e22745c04b8cca1
knoppixmeister <knoppixmeister@gmail.com>
2024-06-26 17:54:33 +0300

    ...

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# RIOT-os-WeAct-STM32F405RG-board
+RIOT os board specification for WeAct STM32F405RGT board
+
+WeAct STM32F405RGT6 board works well with 'stm32f4discovery' board target but its good to have its own board specification.
+NOTE: Blue LED works but other configuration (pins, perips, etc.) requires review by more skilled specialist.
+
+1) Instructions
+
+```
+
+git clone https://github.com/RIOT-OS/RIOT.git
+cd RIOT/boards
+
+git clone https://github.com/knoppixmeister/RIOT-os-WeAct-STM32F405RG-board.git weact-f405rg
+cd ../examples/hello-world
+
+make BOARD=weact-f405rg
+make BOARD=weact-f405rg flash
+
+```
+
+2) Enjoy!
+
+---
+
+Usefull links:
+
+RIOT os code base: https://github.com/RIOT-OS/RIOT.git
+RIOT os stm32f4discovery board code (borrowed code): https://github.com/RIOT-OS/RIOT/tree/master/boards/stm32f4discovery
---
+++ include/board.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_stm32f4discovery
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the STM32F4Discovery evaluation board
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Sebastian Meiling <s@mlng.net>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+#include "periph_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name xtimer configuration
+ * @{
+ */
+#define XTIMER_BACKOFF      (10)
+/** @} */
+
+/**
+ * @name Macros for controlling the on-board LEDs.
+ * @{
+*/
+
+// #define LED0_PIN_NUM        13
+// #define LED0_PORT_NUM       PORT_D
+
+#define LED0_PIN_NUM        2
+#define LED0_PORT_NUM       PORT_B
+
+/** @} */
+
+/**
+ * @name User button
+ * @{
+ */
+#define BTN0_PIN            GPIO_PIN(PORT_A, 0)
+#define BTN0_MODE           GPIO_IN
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "stm32_leds.h"
+
+#endif /* BOARD_H */
+/** @} */
